c3ba3b548fa20b072441c0ce471078545f9c42d4
CHEGEBB <chegephil24@gmail.com>
2026-01-28 17:43:50 +0300

    refactor: Remove unused loading components and related pages

--- app/lecturer/courses/students/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-// Mount the existing lecturer students UI under /lecturer/courses/students
-import StudentsPage from "../../students/page";
-
-export default StudentsPage;
-
--- app/lecturer/courses/submissions/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-// Mount the existing lecturer submissions UI under /lecturer/courses/submissions
-import SubmissionPage from "../../submission/page";
-
-export default SubmissionPage;
-
