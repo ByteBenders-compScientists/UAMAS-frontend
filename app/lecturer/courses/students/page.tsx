ae83e15d4cb584436ceac2bca62acfc7eabff437
CHEGEBB <chegephil24@gmail.com>
2026-01-30 12:39:26 +0300

    initial

---
+++ app/lecturer/courses/students/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+// Mount the existing lecturer students UI under /lecturer/courses/students
+import StudentsPage from "../../students/page";
+
+export default StudentsPage;
+
--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,4 @@
 import type { NextConfig } from "next";
-import withPWAInit from "@ducanh2912/next-pwa";
-
-const withPWA = withPWAInit({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development", // Only disable in dev
-});
 
 const nextConfig: NextConfig = {
   images: {
@@ -15,8 +9,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  staticPageGenerationTimeout: 300,
-  turbopack: {}, // Silence the webpack warning
 };
 
-export default withPWA(nextConfig);
+export default nextConfig;
